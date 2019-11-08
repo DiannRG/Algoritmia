6a6ec23595fecaec0069ce28bcb92a92584792c4
DiannRG <diana.laura.galaviz@gmail.com>
2019-11-08 07:45:54 -0600

    Cambios en estructura

---
+++ Grafo/grafo/Grafo.java
@@ -0,0 +1,13 @@
+
+package grafo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grafo {
+    
+    List<Nodo> Nodos = new ArrayList<>();
+    List<Arista> Aristas = new ArrayList<>();
+    
+    
+}
